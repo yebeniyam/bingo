2c680af8385ac9bf9b57b063e522e3fcd5788da4
benn402 <samrimege@gmail.com>
2025-10-11 15:45:10 -0700

    Change default port to 3002 to avoid conflicts

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Redis configuration - Use environment variables from Render
 const UPSTASH_REDIS_URL = process.env.UPSTASH_REDIS_REST_URL;
